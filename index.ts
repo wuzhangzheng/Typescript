99d6636fc2b052a858efc451cf37a782c7d21d02
吴章政 <wuzhangzheng@hotmail.com>
2022-02-14 08:59:59 +0800

    first commit

---
+++ index.ts
@@ -0,0 +1,9 @@
+let val:string="wzz";
+let str:string="cy";
+let i:number=5;
+function test(a:string[],b:number) {
+    return a.length+b;
+}
+i=test(['6','2','8','5'],5);
+console.log(i)
+console.log(val);
---
+++ js/index.js
@@ -0,0 +1,10 @@
+"use strict";
+let val = "wzz";
+let str = "cy";
+let i = 5;
+function test(a, b) {
+    return a.length + b;
+}
+i = test(['6', '2', '8', '5'], 5);
+console.log(i);
+console.log(val);
